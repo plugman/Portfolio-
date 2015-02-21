10c407db1c6580ff7b7fd03f9479f7749e1978e8
plugman <plugman@imranqadir.me>
2015-02-21 18:46:50 +0500

    Index
    Index

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Portfolio of Imran Qadir, Web Developer | Web Designer</title>
+  
+  <link href="css/style.css" media="all" rel="stylesheet" type="text/css">
+ 
+  <link href="css/css.css" rel="stylesheet" type="text/css">
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57718482-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </head>
+  <body>
+<div id="home">
+    <div id="top_bar_w">
+    <div id="top_bar_i">
+        <ul class="top_menu">
+        <li><a href="#resume_w" data-offset="0">About Me</a></li>
+        <li><a href="#projects_w" data-offset="100">My Work</a></li>
+        <li><a href="#about" data-offset="100">Resume</a></li>
+        <li class="logo"><a href="#resume_w" data-offset="0">&nbsp;</a></li>
+        <li><a href="#services" data-offset="130">Services</a></li>
+        <li><a href="#skills" data-offset="100">Skills</a></li>
+        <li><a href="#contact" data-offset="160">Contact Me</a></li>
+      </ul>
+        <div class="clear"></div>
+      </div>
+  </div>
+    <div id="resume_w">
+    <div class="resume_i">
+        <div class="resume_top">
+        <h1 class="tk-ff-cocon-web-pro">Imran Qadir</h1>
+        <h3 class="sub_name">Web Developer, Web Designer</h3>
+        <div class="contact_links"> <a href="mailto:plugman@imranqadir.me">plugman@imranqadir.me</a> </div>
+      </div>
+        <div class="resume_main">
+        <h2 id="about">About Me</h2>
+        <div class="content_block">
+            <p class="regular"> I'm Imran Qadir, a Lahore/Pakistan based freelance web developer 
+            and designer. I passionately move pixels and lines of code to craft 
+            clean, responsive, and user-friendly websites. My current weapons of 
+            choice are PHP CodeIgniter, Yii, jQuery, and MySQL.
+            Being both an engineer and designer, I help clients bring 
+            their ideas into reality. <a href="#contact" data-offset="160">Contact me</a> today and I would love to hear about your business </p>
+            <!--<p class="regular time_feature"> <span class="time">Time</span> Magazine featured one<br>
+            of the websites I built on their <br>
+            <a href="http://www.time.com/time/specials/packages/article/0,28804,2012721_2012929_2012926,00.html" rel="nofollow" target="_blank">"50 Best Websites of 2010"</a> list </p>-->
+          </div>
+        <h2>Expertise</h2>
+        <h3 class="sub_header">Professional capabilities</h3>
+        <div class="content_block">
+            <ul class="items">
+            <li>High-quality front and back-end development for web sites.</li>
+            <li>Modular, scalable, robust, and reusable code.</li>
+            <li>Performance optimisation, usability, and accessibility.</li>
+            <li>SEO using semantic HTML and Microformats.</li>
+            <li>Site planning and architecture.</li>
+            <li>User interface design.</li>
+          </ul>
+            <h3 class="sub_header" id="skills">Technical skills</h3>
+            <ul class="items">
+            <li>PHP, Codeigniter</li>
+            <li>Yii Framework</li>
+             <li>CubeCart</li>
+              <li>Wordpress</li>
+               <li>opencart</li>
+  
+            <li>Javascript, jQuery, CoffeeScript</li>
+            <li>Photoshop</li>
+            <li>CSS, HTML, SASS, Compass CSS, Blueprint</li>
+            <li>Linux, Apache, SQL, GIT</li>
+          </ul>
+          </div>
+        <h2>Experience</h2>
+        <div class="content_block">
+            <ul class="big_items">
+            <li>
+                <h4>Lead Developer, Designer <span>@WebCreation</span></h4>
+                <p class="info">August 2012–Present</p>
+                <p class="desc">Responsible for Product Design, Development 
+                and Linux Cloud Server Setup. Yii, Cubecart, jQuery, CoffeeScript, 
+                Compass CSS, Server Administration.</p>
+              </li>
+            <li>
+                <h4>Lead Developer, Designer <span>@ModsIndex</span></h4>
+                <p class="info">December 2012–Present</p>
+                <p class="desc">Responsible for Product Design, Development and Linux Cloud Server Setup. Codeigniter PHP, Yii, jQuery, Server Administration.</p>
+              </li>
+            <li>
+                <h4>Lead Developer, Designer <span>@ImeiUnlock</span></h4>
+                <p class="info">February 2012–Present</p>
+                <p class="desc">Responsible for Product Design, Development and Linux Cloud Server Setup. Codeigniter PHP, Yii, jQuery, Server Administration.</p>
+              </li>
+            
+          </ul>
+          </div>
+        <h2>Education</h2>
+        <div class="content_block">
+            <ul class="big_items">
+            <li>
+                <h4>Bahauddin Zakariya University</h4>
+                <p>~ Master of Computer Science / 2010–2012</p>
+              </li>
+          </ul>
+          </div>
+        <div class="footer_links"> <a href="https://www.facebook.com/ImIaMrAn" rel="nofollow">Facebook</a> | <a href="https://www.linkedin.com/pub/imran-qadir/a5/336/809" rel="nofollow">LinkedIn</a> | <a href="mailto:plugman@imranqadir.me">Email</a> </div>
+      </div>
+      </div>
+  </div>
+    <!--<div id="projects_w">
+    <div class="project_w readprint">
+        <div class="project_info_w">
+        <div class="project_info_h">
+            <div class="head blue">
+            <h4>ReadPrint</h4>
+            <a href="http://www.readprint.com/">www.readprint.com</a> </div>
+            <div class="project_info_i">
+            <h5>Description</h5>
+            <p class="text">A free online library, Read Print puts thousands of online books at your fingertips.</p>
+            <h5>Position</h5>
+            <p class="text">Lead web developer/designer</p>
+            <h5>Work</h5>
+            <ul>
+                <li>Design</li>
+                <li>Development (PHP, Codeigniter, MySQL)</li>
+                <li>Cloud Server Setup</li>
+              </ul>
+          </div>
+          </div>
+      </div>
+      </div>
+    <div class="project_w investorwatchdog">
+        <div class="project_info_w">
+        <div class="project_info_h">
+            <div class="head blue">
+            <h4>InvestorWatchdog</h4>
+            <a href="http://www.investorwatchdog.com/">www.investorwatchdog.com</a> </div>
+            <div class="project_info_i">
+            <h5>Description</h5>
+            <p class="text">Online monitoring of investmest risk, performance, &amp; expenses with easy to use personal finance tools.</p>
+            <h5>Position</h5>
+            <p class="text">Lead web developer/designer</p>
+            <h5>Work</h5>
+            <ul>
+                <li>Design</li>
+                <li>Development (Ruby On Rails, MySQL)</li>
+                <li>Payment processor integration</li>
+                <li>Cloud Server Setup</li>
+              </ul>
+          </div>
+          </div>
+      </div>
+      </div>
+    <div class="project_w matchfinancing">
+        <div class="project_info_w">
+        <div class="project_info_h">
+            <div class="head blue">
+            <h4>MatchFinancing</h4>
+            <a href="http://www.matchfinancing.com/">www.matchfinancing.com</a> </div>
+            <div class="project_info_i">
+            <h5>Description</h5>
+            <p class="text">Match Financing is a free service for small and medium sized business borrowers to find business loans.</p>
+            <h5>Position</h5>
+            <p class="text">Lead web developer/designer</p>
+            <h5>Work</h5>
+            <ul>
+                <li>Design</li>
+                <li>Logo Design</li>
+                <li>Development (PHP, Codeigniter, MySQL)</li>
+                <li>Payment processor integration</li>
+                <li>Cloud Server Setup</li>
+              </ul>
+          </div>
+          </div>
+      </div>
+      </div>
+    <div class="project_w buyeyeglasses">
+        <div class="project_info_w">
+        <div class="project_info_h">
+            <div class="head blue">
+            <h4>BuyEyeglasses</h4>
+            <a href="http://www.buyeyeglasses.com/">www.buyeyeglasses.com</a> </div>
+            <div class="project_info_i">
+            <h5>Description</h5>
+            <p class="text">E-commerce website of designer eyewear. Custom made shopping card built from the ground up.</p>
+            <h5>Position</h5>
+            <p class="text">Co-Founder, Lead web developer/designer/seo</p>
+            <h5>Work</h5>
+            <ul>
+                <li>Co-Founder</li>
+                <li>Design</li>
+                <li>Development (PHP, MySQL)</li>
+                <li>Payment processors integration</li>
+                <li>SEO</li>
+              </ul>
+          </div>
+          </div>
+      </div>
+      </div>
+    <div class="project_w zbugs">
+        <div class="project_info_w">
+        <div class="project_info_h">
+            <div class="head blue">
+            <h4>zBugs</h4>
+            <a href="http://www.imranqadir.me/">www.imranqadir.me</a> </div>
+            <div class="project_info_i">
+            <h5>Description</h5>
+            <p class="text">An easy to use online web page compression 
+                tool. Speed up your website by compressing, merging and Gzipping your 
+                CSS and Javascript files.</p>
+            <h5>Position</h5>
+            <p class="text">My Personal Project</p>
+            <h5>Work</h5>
+            <ul>
+                <li>Founder</li>
+                <li>Design</li>
+                <li>Development</li>
+              </ul>
+          </div>
+          </div>
+      </div>
+      </div>
+  </div>-->
+    <div id="services_area">
+    <div class="services_area_i">
+        <div class="box_w services_w">
+        <h3 id="services" class="services_header">Services I offer</h3>
+        <div class="services_w">
+            <div class="service design">
+            <h5>Web design</h5>
+            <p>I simply love web design, all my work starts from a blank 
+                canvas so you will never have a web site that comes from a pre made site
+                template.</p>
+          </div>
+            <div class="service development">
+            <h5>Web development</h5>
+            <p>I ensure all my web code meets the highest web standards that works across all major browsers and is S.E.O friendly.</p>
+          </div>
+            <div class="service seo">
+            <h5>S.E.O</h5>
+            <p>Search Engine Optimization is the most important factor to 
+                your success online and I can help you get top rankings for your target 
+                keywords.</p>
+          </div>
+            <div class="service server">
+            <h5>Linux Server Setup</h5>
+            <p>I'm experienced with setting up, maintaining, and troubleshooting Linux servers.</p>
+          </div>
+          </div>
+      </div>
+      </div>
+    <div class="box_w contact_form_w">
+        <div class="contact_w">
+        <h3 class="box_header icon_mail" id="contact">Contact Me</h3>
+        <div class="success_message" style="display:none">Thank You! Your message has been sent.</div>
+        <form id="contact_form">
+            <div class="field joined">
+            <label>Your name <span>*</span></label>
+            <input id="contact_name" type="text">
+          </div>
+            <div class="field joined">
+            <label>Your email <span>*</span></label>
+            <input id="contact_email" type="text">
+          </div>
+            <div class="clear"></div>
+            <div class="field">
+            <label>Your message <span>*</span></label>
+            <textarea cols="20" rows="7" id="contact_message"></textarea>
+          </div>
+            <div class="buttons_w">
+            <button id="contact_send">Send Message</button>
+          </div>
+          </form>
+        <div class="social_icons"> <span class="find_me_label">Find me on:</span> <a href="https://www.facebook.com/ImIaMrAn" rel="nofollow"><img src="assets/icon-facebook.png" alt=""></a> <a href="https://www.linkedin.com/pub/imran-qadir/a5/336/809" rel="nofollow"><img src="assets/icon-linkedin.png" alt=""></a> <a href="https://twitter.com/ImIaMrAn" rel="nofollow"><img src="assets/icon-twitter.png" alt=""></a> <a href="https://plus.google.com/113046074107439172383/posts" rel="nofollow"><img src="assets/icon-google.png" alt=""></a>
+            <div class="clear"></div>
+          </div>
+      </div>
+      </div>
+  </div>
+    <div id="footer">2014. Portfolio of Imran Qadir. </div>
+  </div>
+<script src="assets/application-24feabaf143cf313dad029ba20d6d931.js" type="text/javascript"></script>
+</body>
+</html>
